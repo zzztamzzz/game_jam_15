81a974cfa3d42028905b1b14616e4e081b13eaa2
Tamzid Choudhury <choudhury.tamzid@gmail.com>
2024-07-26 17:53:21 -0400

    mechanic implemented
    Player character is able to move, shoot, and kill an enemy.
    Extra: Added some invisible walls and a background

---
+++ tutorial/tut_scripts/player_tut.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var speed = 300 # for inspector access
+@export var gravity = 30
+@export var jump_force = 700
+
+# Bullet
+var bullet = preload("res://tutorial/tut_scenes/bullet_tut.tscn")
+var b_inst
+
+func _physics_process(_delta):
+	# Vertical movement
+	# Gravity
+	if !is_on_floor(): # true for on floor and false otherwise
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+	# Jump
+	if Input.is_action_just_pressed("jump"): #&& is_on_floor():
+		velocity.y = -jump_force
+
+	# Horizontal movement
+	var horizontal_direction = Input.get_axis("moveLeft", "moveRight")
+	velocity.x = speed * horizontal_direction
+	
+	shoot()
+	move_and_slide()
+
+func shoot():
+	if Input.is_action_just_pressed("shoot"):
+	#if Input.is_action_pressed("shoot"): # full auto op
+		b_inst = bullet.instantiate()
+		get_parent().add_child(b_inst)
+		b_inst.global_position = $Marker2D.global_position
